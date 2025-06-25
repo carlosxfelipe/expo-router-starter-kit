97f26b3307744b3dae262162d8b6860c34146727
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-06-25 08:41:58 -0300

    Refactor color constants and add MaterialShades for improved theme management

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,35 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
+import { MaterialShades } from "./MaterialShades";
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = MaterialShades.blue[700];
+const tintColorDark = MaterialShades.blue[300];
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#FFFFFF",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
+    surface: "#E6E8EB",
+    status: {
+      success: MaterialShades.green[500],
+      warning: MaterialShades.amber[600],
+      error: MaterialShades.red[600],
+    },
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#ECEDEE",
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
+    surface: "#2C2F31",
+    status: {
+      success: MaterialShades.green[300],
+      warning: MaterialShades.amber[400],
+      error: MaterialShades.red[300],
+    },
   },
 };
